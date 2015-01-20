b2596446e006bbcdf7169920ab2edd75459a4d13
ceesvw123 <ceesvw@hotmail.com>
2015-01-20 15:14:43 +0100

    vesie1

---
+++ Assets/scripts/BaseRotateTurret.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseRotateTurret : MonoBehaviour {
+	private Transform[] transforms;
+	protected Transform Turret;
+	protected Vector3 targetPos;
+	protected Transform nozzle;
+
+
+
+
+	// Use this for initialization
+	protected virtual void Start () {
+
+		transforms = gameObject.GetComponentsInChildren<Transform>();
+		foreach(Transform t in transforms)
+		{
+			if(t.gameObject.name == "Turret")
+			{
+				Turret = t;
+			}
+			if(t.gameObject.name == "nozzle")
+			{
+				nozzle = t;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+
+		Turret.LookAt(targetPos);
+	
+	}
+}
---
+++ Assets/scripts/EnemyShoots.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShoots : MonoBehaviour {
+	private float reloadTime;
+	public float timeToReload;
+	public GameObject bulletPrefab;
+	public float shootingRange;
+	private Transform Turret;
+	private Transform nozzle;
+	
+	// Use this for initialization
+	void Start () {
+		reloadTime = 0;
+		
+		Transform[] transforms = this.gameObject.GetComponentsInChildren<Transform> ();
+		foreach (Transform t in transforms) 
+		{
+			if(t.gameObject.name == "Turret")
+			{
+				Turret = t;
+			}
+			if(t.gameObject.name == "nozzle")
+			{
+				nozzle = t;
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		reloadTime += Time.deltaTime;
+		if (reloadTime >= timeToReload) 
+		{
+			CheckForPlayer();
+			
+			
+		}
+	}
+	private void CheckForPlayer()
+	{
+		
+		Ray myRay = new Ray ();
+		myRay.origin = Turret.position;
+		myRay.direction = Turret.forward;
+		
+		RaycastHit hitInfo;
+		
+		
+		if (Physics.Raycast (myRay, out hitInfo, shootingRange)) 
+		{
+			
+			
+			//hitInfo.collider.gameObject.name;
+			string hitObjectName = hitInfo.collider.gameObject.name;
+			
+			print ("ik rAAk iets");
+			print ("ik raak "+ hitObjectName);
+			if(hitObjectName == "Tank")
+			{
+				
+				Instantiate(bulletPrefab, nozzle.position, nozzle.rotation);
+				
+				reloadTime = 0f;
+			}
+			
+		}
+	}
+}
